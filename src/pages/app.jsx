aaadd203e4c0695a0fba4c7fcc244b03982f5820
Messias Almeida <messiasalmeida9241@gmail.com>
2025-05-26 11:24:46 -0300

    Novas Alterações

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,34 +1,36 @@
 import React from "react";
-import "./links.css"
-import soumaisfitlogo from "./assets/logo.png"
+import "./app.css"
+import logo from "../assets/logo.png"
 
 function App() {
+
     return (
-        <div class="container">
+        < div class="container" >
             <div class="header">
                 <div class="logo-container">
-                    <img src={soumaisfitlogo} alt="soumaisfit-logo" class="logo"></img>
+                    <img src={logo} alt="soumaisfit-logo" class="logo"></img>
                 </div>
                 <h1>Sou + Fit</h1>
                 <p class="slogan">Somos mais que uma academia, somos um complexo</p>
             </div>
 
             <div class="links-grid">
+
+                <div class="link-card">
+                    <a href="">
+                        <div class="link-icon">
+                            <i class="fa solid fa-dumbbell" id="icon-plans"></i>
+                        </div>
+                        Nossos Planos
+                    </a>
+                </div>
+
                 <div class="link-card">
                     <a href="https://www.google.com/maps/place/Academia+Sou+%2B+Fit/@-3.6065567,-38.9647002,17z/data=!3m1!4b1!4m6!3m5!1s0x7c0c35062ae2ccd:0x44dcac8cd6fc45bb!8m2!3d-3.6065621!4d-38.9621253!16s%2Fg%2F11ybtddb_0?entry=ttu&g_ep=EgoyMDI1MDUyMS4wIKXMDSoASAFQAw%3D%3D" target="_blank">
                         <div class="link-icon">
                             <i class="fas fa-map-marker-alt"></i>
                         </div>
                         Localização
-                    </a>
-                </div>
-
-                <div class="link-card">
-                    <a href="http://wa.me/5585992981393?text=Quero%20saber%20mais%20sobre%20os%20planos." target="_blank">
-                        <div class="link-icon">
-                            <i class="fab fa-whatsapp"></i>
-                        </div>
-                        Whatsapp
                     </a>
                 </div>
 
@@ -56,21 +58,9 @@ function App() {
                 <div class="highlight-item">Moderna</div>
                 <div class="highlight-item">Complexo</div>
                 <div class="highlight-item">#SouMaisFit</div>
-                <div class="highlight-item">#SGA</div>
-                <div class="highlight-item">Treino Funcional</div>
                 <div class="highlight-item">Experiência Única</div>
-
-                {/*<div class="highlight-item">Personal Trainers</div>
-                <div class="highlight-item">Moderna</div>
-                <div class="highlight-item">Treino</div>
-                <div class="highlight-item">Alto Padrão</div>
-                <div class="highlight-item">Personal Trainers</div>
-                <div class="highlight-item">Padrão</div>
-                <div class="highlight-item">Padrão</div>
-                <div class="highlight-item">Padrão</div>*/}
             </div>
 
-            {/*} Redes Sociais com Efeito Centralizado {*/}
             <div class="social-links">
                 <a href="https://instagram.com/academiapremium" class="social-icon" target="_blank">
                     <i class="fab fa-instagram"></i>
@@ -89,7 +79,7 @@ function App() {
             <footer>
                 © 2025 Sou + Fit - Todos os direitos reservados
             </footer>
-        </div>
+        </div >
 
     )
 }
