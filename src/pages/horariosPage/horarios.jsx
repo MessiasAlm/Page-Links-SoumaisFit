18a95c71d2d89cfb510907106755ff1b1888d616
Messias Almeida <messiasalmeida9241@gmail.com>
2025-05-28 11:44:53 -0300

    Página de horários

--- src/pages/horariosPage/horarios.jsx
+++ src/pages/horariosPage/horarios.jsx
@@ -4,7 +4,16 @@ import logo from '/src/assets/logo.png';
 
 function horarios() {
   return (
-    <img src={logo} alt="soumaisfit-logo" class="logo"></img>
+    <div class="container">
+      <header>
+        <img src={logo} alt="soumaisfit-logo" class="logo"></img>
+        <h1>Horários</h1>
+        <p class="slogan">Venha conhecer nossa estrutura, será um prazer lhe receber</p>
+      </header>
+      <div class="plan-card">
+
+      </div>
+    </div>
   );
 }
 
