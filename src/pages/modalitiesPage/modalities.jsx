5081671410a2b7f266c3474a097280cc1fc6f9a9
Messias Almeida <messiasalmeida9241@gmail.com>
2025-06-11 15:23:20 -0300

    página de modalidades adicionada

--- src/pages/modalitiesPage/modalities.jsx
+++ src/pages/modalitiesPage/modalities.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
-// import { Container } from './styles';
 
 function modalities() {
   return (
-    <Text>Modalidades</Text>
+    <h1>Modalidades</h1>
   )
 }
 
